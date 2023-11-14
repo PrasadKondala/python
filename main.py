04130d6f9c05830bf813c4b0fe76928193d0a45e
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-14 19:02:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+#write a program  to dedmonstrate different  number data..
+a=10;#integer DT
+b=11.5;#float
+c=2.05j;#complex number
+print("a is Type of ",type(a));
+print("b is Type of ",type(b));
+print("c is Type of ",type(c));
