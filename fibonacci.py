785cfb727b14ad3b5964c3d0dd4ba7394b4085c3
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-15 19:55:32 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,5 @@
+#write a python program to define a  module to find fibonacci numbers and import the module to another program
+import fibonacci
+
+num = int(input("Enter any number to print Fibonacci series: "))
+fibonacci.fib(50) # Removed parentheses around 'num'
