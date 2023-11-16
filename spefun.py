16efece0566782c0efe767e94d2c5f0d19ee86fa
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-11-16 09:47:31 +0530

    Add files via upload

---
+++ spefun.py
@@ -0,0 +1,6 @@
+from arithemetic import Add
+num1=float(input("Enter first Number:"))
+num2=float(input("Enter second Number:"))
+print("Addition is:",Add(num1,num2))
+print("substraction is:",sub(num1,num2))
+
