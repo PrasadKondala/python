c4853fe11b222fdb53efa9464d1e7469511b4aa0
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-12-20 22:10:59 +0530

    Add files via upload

---
+++ arithemetic1.py
@@ -0,0 +1,10 @@
+#arithmetic operations
+a= int(input("enter a value"));
+b=int(input("enter b value"));
+print("addition of a and b",a+b);
+print("substraction of a and b",a-b);
+print("multiple of a and b",a*b);
+print("division of a and b",a/b);
+print("remainder of a and b",a%b);
+print("exponent of a and b",a**b);
+
