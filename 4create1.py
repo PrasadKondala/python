68b1c84d51bee2435cce447aaf9d3ac17fcc68c9
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2023-12-30 17:39:13 +0530

    Add files via upload

---
+++ 4create1.py
@@ -0,0 +1,9 @@
+#write a progrsm to create append,remove list in python
+pets=['cat','tiger','dog','rat',]
+snakes=['python','anaconda','fish','cobra','mamba']
+print("pets are:",pets)
+print("snakes are:",snakes)
+animals=pets+snakes
+print("animals are:",animals)
+snakes.remove("fish")
+print("updated snakes are:",snakes)
