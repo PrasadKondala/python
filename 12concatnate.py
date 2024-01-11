82722fd1042c34989f336dbe043a76a4c1ae9653
PrasadKondala <140306003+PrasadKondala@users.noreply.github.com>
2024-01-11 21:37:25 +0530

    Add files via upload

---
+++ 12concatnate.py
@@ -0,0 +1,7 @@
+s1=input("enter a first string:");
+s2=input("enter a second string:");
+print("first string is:",s1);
+print("second string is:",s2);
+print("concatenations of two string:",s1+s2);
+print("substring of giving string is:",s1[1:4]);
+
